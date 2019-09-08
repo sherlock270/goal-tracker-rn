d1dd443aabd484975577c412f199b32a0e983c7f
CCortinas <crcortinas@gmail.com>
2019-09-07 22:47:04 -0500

    First commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Goal Tracker App</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  text: {
+    color: "purple",
+    fontSize: 69,
+    textAlign: "center"
+  }
+});
